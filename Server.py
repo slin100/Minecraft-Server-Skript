d4851a27b1de57e96bf564418e6412cbe4a3fe96
Nils <46850223+slin100@users.noreply.github.com>
2021-04-12 11:01:00 +0200

    Update Server

--- Server.py
+++ Server.py
@@ -31,6 +31,28 @@ except:
 		ram = input("How much gigabyte ram should have the server? exaple: 1 means 1 gigabyte ram\n")
 		with open("start.bat", "w") as file:
 			file.write(f"java -Xmx{ram}G -jar server.jar nogui")
+		# edit server
+		infile = open('server.properties','r+')
+		content = infile.read()
+		port = input('Change port ["port"/N]')
+		if port.lower() != "n":
+			table = str.maketrans("server-port=25565",f'server-port={port}')
+		seed = input('Change seed ["seed"/N]')
+		if seed.lower() != "n":
+			table = str.maketrans("level-seed=",f'level-seed={seed}')
+		gamemode = input('Change gamemode ["0/1/2/3"/N]')
+		if gamemode.lower() != "n":
+			if gamemode == 1:	gamemode = "creative"
+			elif gamemode == 2: gamemode = "adventure"
+			elif gamemode == 3: gamemode = "spectator"
+			else: gamemode = "survival"
+			table = str.maketrans("gamemode=survival",f'gamemode={gamemode}')
+		name = input('What is the name of the server?\n')
+		table = str.maketrans("motd=A Minecraft Server",f'motd={name}')
+		content = content.translate(table)  #replaces 'foo' with 'bar'
+		infile.write(content)
+		infile.close()
+		print("You can change settings at server.properties")
 		# start server
 		p = subprocess.Popen("start.bat")
 		stdout, stderr = p.communicate()
