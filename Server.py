c62459b61c9e3e8c4b73fabb8b279062fb91fbbd
Nils <46850223+slin100@users.noreply.github.com>
2021-06-08 18:08:22 +0200

    1.17
    Add 1.17 Download to the script

--- Server.py
+++ Server.py
@@ -13,8 +13,8 @@ except:
 		p.wait()
 		os.chdir("Server-Dictionary")
 		# Download link for the server
-		file_url = ["https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar"]
-		whatV = int(input("What server version? 1.16.5[0]\n"))
+		file_url = ["https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar", "https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar"]
+		whatV = int(input("What server version? 1.16.5[0] 1.17[1]\n"))
 		# Download the server.jar
 		print("Downloading")
 		r = requests.get(file_url[whatV], stream = True)
